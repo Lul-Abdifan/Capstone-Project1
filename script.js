2ace6656a24d4eb884071fe278f53145b5460785
A <abdibekele94@gmail.com>
2022-10-26 03:38:43 -0400

    Add speakers object

--- script.js
+++ script.js
@@ -15,3 +15,49 @@ overlay_link.forEach((btn) => {
     btn.onclick = () => {     overlay.style.display = 'none';
     document.body.style.position = 'relative';};
   });
+
+
+  Speakers =[
+    {
+      name:"Dr. Abiy Ahmed Ali",
+      description:"Prime Minister Abiy Ahmed founded INSA or the Information Network Security Agency and became its director from 2008 through 2010. INSAis an organization which spies on people which the government targets it also secures and encrypts Ethiopia's cyber security",
+      position:"Prime Minister of Ethiopia, Nobel Peace Winner , Founder and Former Director of INSA",
+      picture:"./assets/img/speaker1jpeg",
+
+    },
+    {
+      name:"",
+      description:"",
+      position:"",
+      picture:""
+
+    },
+    {
+      name:"",
+      description:"",
+      position:"",
+      picture:""
+
+    },
+    {
+      name:"",
+      description:"",
+      position:"",
+      picture:""
+
+    },
+    {
+      name:"",
+      description:"",
+      position:"",
+      picture:""
+
+    },
+    {
+      name:"",
+      description:"",
+      position:"",
+      picture:""
+
+    },
+  ]
